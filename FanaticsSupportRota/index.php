a22e9d7dfce4feacb1c22dd396c6ac62e53b7b53
Aaron Gagan <a.gagan@edu.salford.ac.uk>
2020-01-10 16:11:20 +0000

    CREATE USER & SIGN IN W/ GENERATED & ENCRYPTED PASSWORDS

--- FanaticsSupportRota/index.php
+++ FanaticsSupportRota/index.php
@@ -1,5 +1,27 @@
 <?php
+if (!isset($_SESSION))
+{
+    session_start();
+}
+
 
 $view = new stdClass();
 $view->pageTitle = 'Support Rota';
+
+require_once ('Models/UserDataSet.php');
+
+if (isset($_POST['signIn'])) {
+    if (isset($_POST['username']) && isset($_POST['psw'])) {
+        $login = new UserDataSet();
+        if ($login->login($_POST['username'], $_POST['psw'])) {
+            $_SESSION['user'] = $_POST['username'];
+            echo $_SERVER['PHP_SELF'];
+        }
+    }
+}
+if (isset($_POST["logout"]))
+{
+    unset($_SESSION["user"]);
+}
+
 require_once('Views/index.phtml');
--- FanaticsSupportRota/maintenance.php
+++ FanaticsSupportRota/maintenance.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+
 $view = new stdClass();
 $view->pageTitle = 'Maintenance';
+
+if (isset($_POST['b'])) {
+    $login = new UserDataSet();
+    $login->createUser($_POST['username'], $_POST['team']);
+}
+
 require_once('Views/maintenance.phtml');
