2b1b6f0c01aad1a91b9d99a13990f85f8b3819d0
Luke Wood <lukewood.projects@gmail.com>
2020-01-13 17:27:05 +0000

    Manual updating now works. Still need to add automatic refresh and some other small bits, but the main feature is complete
    Signed-off-by: Luke Wood <lukewood.projects@gmail.com>

--- FanaticsSupportRota/js/maintenance_screen.js
+++ FanaticsSupportRota/js/maintenance_screen.js
@@ -35,12 +35,15 @@ function handleDeveloperRotaDrop(event, ui) {
 function removeDeveloperFromSupport(index)
 {
     // Get name from current developer in slot
+    //var name = $("#rotaSlot" + index)[0].children[0].children[0].innerText;
     var name = $("#rotaSlot" + index)[0].children[0].children[0].innerText;
+    var name = name.trim();
+    console.log(name);
+    console.log(name === "Billy");
 
     // Find the dev card with the username and display it again
     $(".draggableDevelopers:contains(" + name + ")")[0].style.display = "block";
-
-    // Set the rota slot to red, don't show trash button, say developer required
+    // // Set the rota slot to red, don't show trash button, say developer required
     $("#rotaSlot" + index)[0].style.backgroundColor = "red";
     $("#rotaSlot" + index)[0].children[0].children[0].innerText = "** Developer Required **";
     $("#rotaSlot" + index)[0].children[0].children[1].style.display = "none";
@@ -48,17 +51,26 @@ function removeDeveloperFromSupport(index)
 
 function updateRota() {
     var modified_rota = {};
-    $("#supportTeamColumn").children("#supportTeam").each(function() {
+    $("#supportTeamColumn").children("#supportTeam").each(function(index) {
         var date_start = $(this).find("#startDate")[0].innerText;
         var dev1 = $(this).find(".card-body")[0].children[0].innerText;
         var dev2 = $(this).find(".card-body")[0].children[1].innerText;
         var dev_array = [];
+        dev_array.push(date_start);
         dev_array.push(dev1);
         dev_array.push(dev2);
         //console.log(date_start);
         // console.log(dev1);
         // console.log(dev2);
-        modified_rota[date_start] = dev_array;
+        modified_rota[index] = dev_array;
     });
     //console.log(modified_rota);
+    $.ajax({
+        url: "../updateRota.php",
+        type: "post",
+        data: {rota: modified_rota},
+        success: function(){
+            console.log("Success");
+        }
+    })
 }
--- FanaticsSupportRota/updateRota.php
+++ FanaticsSupportRota/updateRota.php
@@ -1,3 +1,8 @@
 <?php
 require_once("Models/SupportTeamDataSet.php");
 $supportTeamObject = new SupportTeamDataSet();
+$data = $_POST['rota'];
+foreach ($data as $supportWeek)
+{
+    $supportTeamObject->updateSlot($supportWeek[1], $supportWeek[2], date("Y-d-m", strtotime($supportWeek[0])));
+}
