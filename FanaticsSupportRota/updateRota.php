7e21240f34f9ac846192028ac2f1bc0303b7389e
Luke Wood <lukewood.projects@gmail.com>
2020-01-14 12:48:46 +0000

    I'm honestly not sure
    Signed-off-by: Luke Wood <lukewood.projects@gmail.com>

--- FanaticsSupportRota/updateRota.php
+++ FanaticsSupportRota/updateRota.php
@@ -4,5 +4,7 @@ $supportTeamObject = new SupportTeamDataSet();
 $data = $_POST['rota'];
 foreach ($data as $supportWeek)
 {
-    $supportTeamObject->updateSlot($supportWeek[1], $supportWeek[2], date("Y-d-m", strtotime($supportWeek[0])));
+//    $startDate = date("Y-d-m", strtotime($supportWeek[0]));
+    $supportTeamObject->updateSlot($supportWeek[1], $supportWeek[2], $supportWeek[0]);
+    //$startDate = date("Y-m-d", strtotime($supportWeek[0]));
 }
